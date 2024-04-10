58a978fb6225bee82c612019cdd34b6e80f0059a
Ali <aligr.worku@gmail.com>
2024-04-10 16:43:38 +0330

    first shemas of blog, category and comment models

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from users.models import User
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    parent = models.ForeignKey(
+        'self', blank=True, null=True, on_delete=models.CASCADE)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    reply_to = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
