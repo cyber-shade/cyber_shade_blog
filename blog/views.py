25cb6fa2e6242254dee896228446c2e4e74ff245
Ali <aligr.worku@gmail.com>
2024-04-11 19:32:47 +0330

    i just putted a template for website and fixing blog list and blog single pages

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Blog, Comment
+from .models import Blog, Category, Comment
+from users.models import User
 # Create your views here.
 
 
 def blog_list(request):
     blogs = Blog.objects.all()
-    return render(request, 'blog/blogs.html', {'blogs': blogs, 'category': 'all'})
+    categories = Category.objects.all()
+    return render(request, 'blogs.html', {'blogs': blogs, 'categories': categories})
 
 
 def detail(request, id):
     blog = get_object_or_404(Blog, pk=id)
+    author_details = User.objects.get(username=blog.author)
     comments = Comment.objects.filter(blog=blog)
-    return render(request, 'blog/detail.html', {'blog': blog, 'comments': comments})
+    return render(request, 'blog-single.html', {'blog': blog, 'author': author_details, 'comments': comments})
--- cyber_shade_blog/settings.py
+++ cyber_shade_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'cyber_shade_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,9 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = 'users.User'
