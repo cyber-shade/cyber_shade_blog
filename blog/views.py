77591a714bcdfb9f2d48ffd7ecff5c9eb2431022
Ali <aligr.worku@gmail.com>
2024-04-12 21:18:00 +0330

    created a header template and include it in all templates, fixing some bugs in blog apps, creating new template fo categories

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,19 @@ from users.models import User
 # Create your views here.
 
 
-def blog_list(request):
-    blogs = Blog.objects.all()
-    categories = Category.objects.all()
-    return render(request, 'blogs.html', {'blogs': blogs, 'categories': categories})
+def categories(request):
+    return render(request, 'categories.html')
 
 
-def detail(request, id):
+def blogs(request, category_id: int = None):
+    if category_id is not None:
+        blogs = Blog.objects.filter(category=category_id)
+    else:
+        blogs = Blog.objects.all()
+    return render(request, 'blogs.html', {'blogs': blogs})
+
+
+def single_blog(request, id):
     blog = get_object_or_404(Blog, pk=id)
     author_details = User.objects.get(username=blog.author)
     comments = Comment.objects.filter(blog=blog)
