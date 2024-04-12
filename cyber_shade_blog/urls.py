93f5ec2d7852fd84d975d8e6ab3bb4b61baf5007
Ali <aligr.worku@gmail.com>
2024-04-12 18:21:32 +0330

    i created login and register templates and a very simple one for profile. i created the login, logout, register and profile views. and also edit the neccessary file for making this changes

--- cyber_shade_blog/urls.py
+++ cyber_shade_blog/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog import urls as blog_urls
+from users import urls as users_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include(blog_urls)),
+    path('', include(users_urls)),
 ]
