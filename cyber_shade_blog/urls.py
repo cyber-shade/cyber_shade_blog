d64a794d13ff4122949d534af8d89ac6d2a9253d
Ali <aligr.worku@gmail.com>
2024-04-14 16:17:19 +0330

    Blog model now has a cover and CKEditor is used in it. some unnecessary files has been deleted!

--- cyber_shade_blog/urls.py
+++ cyber_shade_blog/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from blog import urls as blog_urls
 from users import urls as users_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include(blog_urls)),
     path('', include(users_urls)),
+
 ]
+urlpatterns += [
+    path("ckeditor5/", include('django_ckeditor_5.urls'),
+         name="ck_editor_5_upload_file"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
